f850b68985403fd18ab744e8007ae766655dc60b
Ashraful Haque <devvorb@gmail.com>
2025-08-29 21:27:10 +0600

    init social media following project

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+// env vars
+dotenv.config();
+
+const PORT = process.env.PORT || 9090;
+
+// init app
+const app = express();
+
+// supports
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routing
+
+// server listen
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`.bgCyan.black);
+});
